e11e416835937d07d01c181eeed1fc31cc48ee1a
simon435500 <simon435500@users.noreply.github.com>
2017-10-12 12:02:44 +0800

    new script for install docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y apt-transport-https ca-certificates
-apt-key adv \
-  --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-  --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
 apt-get update
-
-apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt-get install -y docker-engine
-gpasswd -a $USER docker
-service docker restart
+apt-get install docker-ce
